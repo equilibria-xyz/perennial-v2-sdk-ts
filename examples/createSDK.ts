45c18c74165c309639cb3ec2aac1710fd4766edc
Arjun Rao <2940142+arjun-io@users.noreply.github.com>
2024-10-24 17:12:13 -0400

    update npmrc, fix example

--- examples/createSDK.ts
+++ examples/createSDK.ts
@@ -1,9 +1,9 @@
 import { config as dotenvConfig } from 'dotenv'
 import path from 'path'
 import { getAddress } from 'viem'
-import { arbitrum, arbitrumSepolia } from 'viem/chains'
+import { arbitrum } from 'viem/chains'
 
-import Perennial, { ChainMarkets, Day, PositionSide, SupportedMarket } from '../'
+import Perennial, { SupportedMarket } from '../'
 
 dotenvConfig({ path: path.resolve(__dirname, '../.env.local') })
 ;(BigInt.prototype as any).toJSON = function () {
@@ -11,11 +11,11 @@ dotenvConfig({ path: path.resolve(__dirname, '../.env.local') })
 }
 
 async function run() {
-  const markets = [SupportedMarket.eth /* , SupportedMarket.xau, SupportedMarket.btc, SupportedMarket.sol */]
+  const markets = [SupportedMarket.eth, SupportedMarket.xau, SupportedMarket.btc, SupportedMarket.sol]
   const address = getAddress('0x1deFb9E9aE40d46C358dc0a185408dc178483851')
   const sdk = new Perennial({
-    chainId: arbitrumSepolia.id,
-    rpcUrl: process.env.RPC_URL_ARBITRUM_SEPOLIA!,
+    chainId: arbitrum.id,
+    rpcUrl: process.env.RPC_URL_ARBITRUM!,
     graphUrl: process.env.GRAPH_URL_ARBITRUM_NEW!,
     pythUrl: process.env.PYTH_URL!,
     cryptexUrl: process.env.CRYPTEX_URL!,
@@ -23,10 +23,7 @@ async function run() {
     supportedMarkets: markets,
   })
 
-  console.log(
-    await sdk.markets.read.settlementFees(),
-    await sdk.markets.read.settlementFees({ markets: [SupportedMarket.meem] }),
-  )
+  console.log(await sdk.markets.read.marketSnapshots())
 }
 
 run()
