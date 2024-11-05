089bfe9614cbb7bb3b3be6c33a414570dcda7991
Arjun Rao <2940142+arjun-io@users.noreply.github.com>
2024-11-05 10:35:38 -0500

    Enable SupportedMarket enum or address as intent arg

--- src/lib/collateralAccounts/intent/buildMarketTransferSigningPayload.ts
+++ src/lib/collateralAccounts/intent/buildMarketTransferSigningPayload.ts
@@ -1,26 +1,29 @@
 import { Address } from 'viem'
 
 import { EIP712_Domain, MarketTransferSigningPayload, MarketTransferSigningTypes } from '../../../constants/eip712'
+import { SupportedMarket } from '../../../constants/markets'
 import { CommonOverrides, CommonRequired } from '../../../types/shared'
+import { addressForMarket } from '../../../utils/addressUtils'
 import { ActionRequred, buildActionMessage } from './_util'
 
 export type BuildMarketTransferSigningPayloadArgs = CommonRequired &
   ActionRequred &
   CommonOverrides & {
-    market: Address
+    market: Address | SupportedMarket
     amount: bigint
   }
 export function buildMarketTransferSigningPayload({
   chainId,
   address: account,
   maxFee,
-  market,
+  market: market_,
   amount,
   overrides,
   expiry,
 }: BuildMarketTransferSigningPayloadArgs): {
   marketTransfer: MarketTransferSigningPayload
 } {
+  const market = addressForMarket(chainId, market_)
   const message = {
     market,
     amount,
